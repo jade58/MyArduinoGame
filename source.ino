e70ed802ccadeb00ff93ee86aa0adb2199b83125
Jade Wizard <buryakovdima@yandex.ru>
2015-07-23 21:18:56 +0300

    Update source.ino

--- source.ino
+++ source.ino
@@ -1,6 +1,6 @@
-//DFRobot.com
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
+//My first game for adruino
+//Author - JadeWizard//
+//Blog - JadeWizard.ru
 
 int btnStep_player1 = 2;
 int btnOk_player1 = 3;
@@ -37,13 +37,30 @@ void loop()
   //И запускаем процедуру генерации случайного чилса.
   
   if (step == 1) {
-    lcd.init(); //Инициализурем дисплей
-    lcd.backlight; //Подсветка дисплея
-    
+
     digitalWrite(led_player1,HIGH);
-    lcd.cursor(0,1); //Позиция курсора - 2 строка(нумерация начинается с нуля).
+    digitalWrite(led_player2,LOW);
+    
+    lcd.init(); //Инициализурем дисплей
+    lcd.backlight(); //Подсветка дисплея
+    
+    lcd.print(10); //Выводим число
+    lcd.setCursor(0,1); //Позиция курсора - 2 строка(нумерация начинается с нуля).
 
     lcd.print("Step: Player #1");
+    
+ } else {
+    
+    digitalWrite(led_player2,HIGH);
+    digitalWrite(led_player1,LOW);
+
+    lcd.init(); //Инициализируем дисплей
+    lcd.backlight(); //Подсветка
+
+    lcd.print(10); //Выводим число
+    
+    lcd.setCursor(0,1); //Смещаем курсор
+    lcd.print("Step: Player #2"); //Номер игрока
     
   }
   
