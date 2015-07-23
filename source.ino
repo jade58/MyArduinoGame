80f16b11d4f5a7ef737267976c1802322bd9bde2
Jade Wizard <buryakovdima@yandex.ru>
2015-07-23 22:09:06 +0300

    Update source.ino

--- source.ino
+++ source.ino
@@ -9,6 +9,17 @@ int btnOk_player2 = 4;
 int led_player1 = 8;
 int led_player2 = 9;
 
+int u_step = 1; //Текущий ход игрок #1 ходит первый.
+
+int num1 = 0; //Числа первого игрока
+int num2 = 0; //Число второго игрока.
+
+int p_step1 = 0; //Кол-во промежуточных шагов.
+int p_step2 = 0; //Кол-во промежуточных шагов второго игрока.
+
+int step1 = 0; //Общее кол-во шагов.
+int step2 = 0; //Кол-во промежуточных шагов
+
 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
@@ -24,30 +35,59 @@ void setup()
   pinMode(btnOk_player1,INPUT); //Кнопка ОК #1
   pinMode(btnStep_player2,INPUT); //Кнопка генерации числа #2
   pinMode(btnOk_player2,INPUT); //Кнопка ОК #2
+  
+}
 
+//Функция генерации рандомного числа для игроков.
+
+int rnd_number () {
+  
+  int number;
+
+  number = random(48);
+ 
+  return(number);
+  
 }
 
 void loop()
 {
 
-  int step = 1; //Текущий ход игрок #1 ходит первый.
-  
+  int rnd = 0; //Рандомное число.
+
   //Если ходит первый игрок, то светодиод 1 = максимум
   //На экране выводим сообщение о том, что ходит игрок RED(#1)
   //И запускаем процедуру генерации случайного чилса.
-  
-  if (step == 1) {
 
-    digitalWrite(led_player1,HIGH);
-    digitalWrite(led_player2,LOW);
-    
     lcd.init(); //Инициализурем дисплей
     lcd.backlight(); //Подсветка дисплея
     
-    lcd.print(10); //Выводим число
     lcd.setCursor(0,1); //Позиция курсора - 2 строка(нумерация начинается с нуля).
 
     lcd.print("Step: Player #1");
+  
+  if (u_step == 1) {
+    
+    if (digitalRead(btnStep_player1) == HIGH) {
+      rnd = rnd_number (); //Число первого игрока
+      num1 = num1 + rnd; //Подсчитываем общее число.
+      p_step1 = p_step1 + 1; //число промежуточных ходов
+    }
+
+    if (num1 != 0 && p_step1 > 1 && digitalRead(btnOk_player1) == HIGH) {
+      step1 = step1 + 1; //Число общих ходов;
+      u_step = 2;
+    }
+
+    if (p_step1 == 3) {
+      step1 = step1 + 1; //Число общих ходов
+      u_step = 2;
+    }
+
+    digitalWrite(led_player1,HIGH);
+    digitalWrite(led_player2,LOW);
+        
+    lcd.print(num1); //Выводим число
     
  } else {
     
@@ -63,5 +103,5 @@ void loop()
     lcd.print("Step: Player #2"); //Номер игрока
     
   }
-  
+ 
 }
